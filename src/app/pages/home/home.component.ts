ad50b465116c3a32779ad1c450d4411cd826b3a0
JoseTemox <temistoclesledezma@gmail.com>
2021-04-12 13:12:02 -0400

    carga servicio en consola

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from '../../services/youtubeService.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private youtobeService: YoutubeService) { }
 
   ngOnInit(): void {
+
+    this.youtobeService.getVideos().subscribe( videos =>{
+      console.log(videos);
+    })
   }
 
 }
---
+++ src/app/services/youtubeService.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient,HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { YoutobeResponse } from '../models/youtobeModels';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+
+  private youtobeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apikey = 'AIzaSyDr4mlfJIX9d5sqtnA69bTdjkfg9ryLc6o';
+  private playlist = 'UU4UQFLK99sYuG1b8oRd5BvQ';
+  private nextPageToken = '';
+
+
+
+
+  constructor( private http: HttpClient) {
+
+  }
+
+  getVideos(){
+
+    const url = `${this.youtobeUrl}/playlistItems`;
+    const params = new HttpParams()
+      .set('part','snippet')
+      .set('maxResults','10')
+      .set('playlistId', this.playlist)
+      .set('key', this.apikey)
+
+    return this.http.get<YoutobeResponse>( url, { params}).pipe(
+      map( resp => {
+        this.nextPageToken = resp.nextPageToken
+        return resp.items;
+      }),
+      map( items => items.map( video => video.snippet ))
+    )
+
+  }
+
+
+}
